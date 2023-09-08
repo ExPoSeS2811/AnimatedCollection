0864ddfa418050b96af9377450fc40dcc122649c
Gleb Rasskazov <glebunium146@gmail.com>
2023-09-08 19:13:31 +0300

    initial commit

---
+++ AnimatedCollection/AnimatedCollection/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedCollection
+//
+//  Created by Gleb Rasskazov on 08.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
